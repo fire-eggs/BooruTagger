4e4c4760d40a889d84f87f057c7191f3eac70a22
fire-eggs <lifeattickville@gmail.com>
2015-01-17 14:51:46 -0500

    Actually rename files on tag changes

--- ImageFile.cs
+++ ImageFile.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
 
 // Represents a single image
-using System.Windows.Markup;
+
+// TODO tag character as an option
+// TODO previewURL -> a preview thumbnail image. Need to store original path separately.
 
 namespace ImageTag
 {
@@ -18,6 +17,8 @@ namespace ImageTag
         private string _dimensions;
         private List<string> _tagList;
 
+        private string _baseFile;
+
         public ImageFile(string filePath)
         {
             PreviewURL = filePath;
@@ -25,7 +26,7 @@ namespace ImageTag
             _tagList = MakeTags();
         }
 
-        public string PreviewURL
+        public string PreviewURL 
         {
             get { return _previewURL; }
             set
@@ -56,11 +57,14 @@ namespace ImageTag
             }
         }
 
-        private static char[] tagSplit = new char[] { '+' };
+        private static char[] tagSplit = { '+' };
         private List<string> MakeTags()
         {
             var fileN = Path.GetFileNameWithoutExtension(_previewURL);
             var bits = fileN.Split(tagSplit);
+
+            _baseFile = bits[0];
+
             var tagList = new List<string>();
             for (int i = 1; i < bits.Length; i++)
                 tagList.Add(bits[i]);
@@ -79,30 +83,63 @@ namespace ImageTag
 
         public void RemoveTags(IList tagsToRemove)
         {
+            bool anyHits = false;
             foreach (var tag in tagsToRemove)
             {
-                _tagList.Remove(tag as string);
+                anyHits |= _tagList.Remove(tag as string);
             }
 
-            // TODO actually change the filename!
+            if (anyHits)
+                RenameFile();
         }
 
         public void AddTag(string tag)
         {
             if (!_tagList.Contains(tag))
+            {
                 _tagList.Add(tag);
-
-            // TODO actually change the filename!
+                RenameFile();
+            }
         }
 
         public void ChangeTag(string oldtag, string newtag)
         {
+            bool anyChange = false;
             if (_tagList.Contains(oldtag))
+            {
                 _tagList.Remove(oldtag);
+                anyChange = true;
+            }
             if (!_tagList.Contains(newtag))
+            {
                 _tagList.Add(newtag);
+                anyChange = true;
+            }
 
-            // TODO actually change the filename!
+            if (anyChange)
+                RenameFile();
+        }
+
+        private void RenameFile()
+        {
+            string filename = _baseFile;
+            foreach (var aTag in _tagList)
+            {
+                filename += "+" + aTag;
+            }
+
+            string invalid = new string(Path.GetInvalidFileNameChars()) + new string(Path.GetInvalidPathChars());
+            foreach (char c in invalid)
+            {
+                filename = filename.Replace(c.ToString(), "");
+            }
+
+            string dest = Path.Combine(Path.GetDirectoryName(_previewURL),filename) + Path.GetExtension(_previewURL);
+
+            // TODO check if dest length > 256
+
+            File.Move(_previewURL, dest);
+            _previewURL = dest;
         }
 
     }
