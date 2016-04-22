f8ed8ebed743bd487dbbd08585dfa25a608196fd
fire-eggs <lifeattickville@gmail.com>
2016-04-22 12:54:03 -0400

    filter illegal filename characters when adding/changing tags

--- AddTagDlg.xaml.cs
+++ AddTagDlg.xaml.cs
@@ -1,19 +1,27 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+
+// TODO are the AddTag and ChgTag dialogs *the same* ?
 
 namespace ImageTag
 {
     /// <summary>
-    /// Interaction logic for AddTagDlg.xaml
+    /// Add a tag to an image. User can select from an existing tag or type in a new tag.
     /// </summary>
     public partial class AddTagDlg : Window
     {
+        private readonly char[] _illegalChars;
+
         public AddTagDlg(IEnumerable<string> tags)
         {
             InitializeComponent();
             taglist.ItemsSource = tags;
             btnDlgOK.IsEnabled = true; // TODO find "on text changed" event for editable combo
+            _illegalChars = Path.GetInvalidFileNameChars();
         }
 
         private void Taglist_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -26,6 +34,12 @@ namespace ImageTag
             DialogResult = true;
         }
 
-        public string Answer { get { return taglist.Text as string; } }
+        public string Answer { get { return taglist.Text; } }
+
+        private void Taglist_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (_illegalChars.Contains(e.Text[0]) || e.Text[0] == '+') // plus is special: tag separator
+                e.Handled = true;
+        }
     }
 }
--- ChgTagDlg.xaml.cs
+++ ChgTagDlg.xaml.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
-
-// TODO filter for illegal path/file characters at combobox level
+using System.Windows.Input;
 
 namespace ImageTag
 {
@@ -11,6 +12,8 @@ namespace ImageTag
     /// </summary>
     public partial class ChgTagDlg : Window
     {
+        private readonly char[] _illegalChars;
+
         public ChgTagDlg(string oldTag, IEnumerable<string> tags)
         {
             InitializeComponent();
@@ -18,6 +21,8 @@ namespace ImageTag
             btnDlgOK.IsEnabled = true; // TODO find "on text changed" event for editable combo
             OldTag.Text = string.Format("Changing tag '{0}'", oldTag);
             taglist.SelectedItem = oldTag;
+
+            _illegalChars = Path.GetInvalidFileNameChars();
         }
 
         private void Taglist_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -27,9 +32,16 @@ namespace ImageTag
 
         private void BtnDlgOK_OnClick(object sender, RoutedEventArgs e)
         {
+            // TODO need to double-check for illegal characters? i.e. text pasted from clipboard isn't filtered?
             DialogResult = true;
         }
 
         public string Answer { get { return taglist.Text; } }
+
+        private void Taglist_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (_illegalChars.Contains(e.Text[0]) || e.Text[0] == '+') // plus is special: tag separator
+                e.Handled = true;
+        }
     }
 }
